1e6009c5afdd8fcd3f0f81b3054f553ee1059770
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-12-25 16:57:06 +0400

    Add files via upload

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,93 @@
 <template>
   <div class="container">
+    <div class="navbar">
     <div class="navbar-bav" v-if="isLogged">
-      <router-link to="/">Dashboard</router-link>
-      <router-link to="/friends">Друзья</router-link>
-      <router-link to="/chats">Переписки</router-link>
-      <router-link v-if="user.is_admin == 1" to="/admin">Админ</router-link>
-      <a href="#" @click="logout">logout</a>
-      <a :href="$router.resolve({ name: 'userPage', params: { id: user.id } }).href">Личка</a>
+      <div class="logo_div">
+      <a href="/"><img src="/storage/img/logo.png"></a>
+      <p>Vobla</p>
+      </div>
+      <div class="user_nav_div">
+      <a href="/chats">Диалоги</a>
+      <a href="/friends">Друзья</a>
+      <a href="/admin" v-if="is_admin == 1">Админ</a>
+      <a href="#" @click="logout">Выйти</a>
+      <a :href="$router.resolve({ name: 'userPage', params: { id: user.id } }).href"><img :src="'/storage/profile_pics/' + user.avatar" class="user_avatar"></a>
+      </div>
       </div>
     <div class="" v-else>
-      <router-link to="/">Home</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/registration">Register</router-link>
-    </div>
+      <div class="logo_div">
+      <a href="/"><img class="logo" src="/storage/img/logo.png"></a>
+      <p>Vobla</p>
+      </div>
+      <div class="login_register_div">
+      <a href="/login" class="login_nav">Авторизация</a>
+      <a href="/registration">Регистрация</a>
+      </div>
 
+    </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
+
+<style>
+body {
+  background: url(/storage/img/background.png);
+  background-size: 100%;
+  background-position-x: center;
+}
+
+a {
+  text-decoration: none;
+}
+
+.navbar {
+  background-color: #EBB3C0;
+  height: 165px;
+}
+
+.login_register_div {
+  text-align: end;
+  position: relative;
+  bottom: 5vh;
+}
+
+.login_register_div a {
+  margin-right: 3%;
+  font-size: 32px;
+  color: black;
+}
+
+.logo_div {
+  font-size: 32px;
+  position: relative;
+  margin-left: 2%;
+  top: 2vh;
+}
+
+.user_avatar {
+  width: 80px;
+  height: 80px;
+  border-radius: 100%;
+  position: relative;
+  top: 2vh;
+}
+
+.user_nav_div {
+  text-align: end;
+  position: relative;
+  bottom: 6vh;
+}
+
+.user_nav_div a {
+  margin-right: 6%;
+  font-size: 32px;
+  color: black;
+  position: relative;
+  bottom: 3vh;
+}
+
+</style>
 
 <script>
 export default {
@@ -24,13 +95,21 @@ export default {
   data() {
     return {
       isLogged: false,
-      user: []
+      user: [],
+      is_admin: false
     };
   },
   created() {
     if (IsLogged) {
       this.isLogged = true;
-      this.user = window.Laravel.user
+      this.is_admin = window.Laravel.user.is_admin
+      this.$axios
+        .get("http://127.0.0.1:8000/api/userinfo")
+        .then((response) => {
+          this.user = response.data;
+          console.log(this.user)
+
+        });
     }
   },
   methods: {
